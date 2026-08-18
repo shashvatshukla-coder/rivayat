8460ba9ea82c1d30b14ec25a5f3296095c7617ed
shashvatshukla-coder <shashvatshukla240@gmail.com>
2026-08-18 16:46:35 +0530

    cleanup: remove extra backend wrapper

--- modern-server.js
+++
@@ -1,8 +0,0 @@
-// Compatibility wrapper for older Render/Vercel start commands.
-// All backend models, routes, auth, launch APIs and seeding now live in server.js.
-const { startApplication } = require("./server");
-
-startApplication().catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
